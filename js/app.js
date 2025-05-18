e32bfc83579c72d25bef629682d6885af2760b14
Carlos Calel <ccalelc2@gmail.com>
2025-05-17 20:32:44 -0600

    javascript añadido

--- js/app.js
+++
@@ -1,6 +0,0 @@
-const hamburguesa = document.querySelector(".hamburguesa");
-const navMenu = document.querySelector("nav ul");
-
-hamburguesa.addEventListener("click", () => {
-    navMenu.classList.toggle("verMenu");
-});
